52f63cad3012472f39b89bb9b54b103a3ad807f0
BeardedBear <germain.poirrier@gmail.com>
2020-04-03 11:10:01 +0200

    Add void theme.

--- src/main.js
+++ src/main.js
@@ -204,3 +204,31 @@ makeTheme(
         }
     )
 );
+
+makeTheme(
+    "void",
+    Object.assign(
+        {
+            primary: "#7A63ED",
+            primaryalt: "#2A2156",
+            uiborder: "#1A1430",
+            uibackground: "#150F23",
+            uibackgroundalt: "#0E0814",
+            default: "#D7E2EF"
+        },
+        {
+            blue: "#7A63ED",
+            green: "#6DBBFF",
+            greenalt: "#2C729E",
+            grey: "#7F7070",
+            orange: "#3D8DE2",
+            pink: "#A8547A",
+            purple: "#3BA7BA",
+            red: "#C13838",
+            salmon: "#D65170",
+            transparent: "#00000000",
+            turquoize: "#89C4FF",
+            yellow: "#CC6AC5"
+        }
+    )
+);
