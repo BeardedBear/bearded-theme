88bc8473754db105a09a24b97c43f88eb7c4eb70
BeardedBear <germain.poirrier@gmail.com>
2023-03-19 01:17:00 +0100

    Fix medium font colors

--- src/helper.ts
+++ src/helper.ts
@@ -12,7 +12,13 @@ interface ThemeProps {
   reversed?: boolean;
 }
 
-export function makeMainColors({ base, primary, primaryAlt, fontTeinted, reversed }: ThemeProps): ThemeUi {
+export function makeMainColors({
+  base,
+  primary,
+  primaryAlt,
+  fontTeinted,
+  reversed,
+}: ThemeProps): ThemeUi {
   const darkenColor: string = c(base).darken(0.022).toHex();
   const sat = 0.15;
 
@@ -25,12 +31,14 @@ export function makeMainColors({ base, primary, primaryAlt, fontTeinted, reverse
     primaryalt: primaryAlt ? primaryAlt : c(base).lighten(0.05).toHex(),
     uiborder: c(base).darken(0.06).toHex(),
     // Fonts
-    default: fontTeinted ? c(base).lighten(0.65).saturate(0.15).toHex() : c(base).lighten(0.7).desaturate(sat).toHex(),
+    default: fontTeinted
+      ? c(base).lighten(0.6).saturate(0.15).toHex()
+      : c(base).lighten(0.7).desaturate(sat).toHex(),
     defaultMain: fontTeinted
       ? c(base).lighten(0.55).saturate(0.07).toHex()
-      : c(base).lighten(0.5).desaturate(0.15).toHex(),
+      : c(base).lighten(0.65).desaturate(sat).toHex(),
     defaultalt: fontTeinted
       ? c(base).lighten(0.15).saturate(0.05).toHex()
-      : c(base).lighten(0.2).desaturate(0.15).toHex(),
+      : c(base).lighten(0.25).desaturate(sat).toHex(),
   };
 }
